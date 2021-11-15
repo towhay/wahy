19a3afe7b4c6d29455bf2ae966834621aee572a3
towhay <93511970+towhay@users.noreply.github.com>
2021-11-15 21:39:40 +0700

    Initialize dependabot/npm_and_yarn/ts-node-tw-10.4.0

---
+++ config.js
@@ -0,0 +1,27 @@
+userbot = {
+ owner: [ "62857420881846","628812904283","6282328303332", "62822980698995", "436504463151666", "62823283033323" ],
+  MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
+   mess: {
+   wait: "tunggu sedang di proses...",
+   error: "errorr!!"
+   },
+   prefix: ["/"],
+   gexp: 50,
+   limit: 100,
+   eror: "Sepertinya Eror",
+   waits: "Sedang di proses",
+   butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU', // buat Image Button
+   packname: '༻𝙍𝙞𝙭𝙡𝙚𝘽𝙤𝙩༺',
+   author: '@_RizkyAdi',
+    setting: {
+    admin: "only admin",
+    group: "only group",
+    owner: "owner only pack",
+    jadibot: "jadibot only",
+    botadmin: "bot harus menjadi admin",
+    nsfw: "FITUR BELUM DIAKTIFKAN!"
+    }
+}
+module.exports = userbot
+
+
